a964305b0d5629fdbdab7b2d4d16df4111ca6b6d
Vivek1998-G <83626875+Vivek1998-G@users.noreply.github.com>
2022-12-09 13:22:05 +0530

    Update axios.js

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const instance = axios.create({baseURL:"https://jumpsuit-agouti.cyclic.app/v2/posts"})
+const instance = axios.create({baseURL:"https://jumpsuit-agouti.cyclic.app"})
 
 export default instance

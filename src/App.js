2b06eb58abda7d3fb16c93747c28b05fe9e84513
Vivek1998-G <83626875+Vivek1998-G@users.noreply.github.com>
2022-12-25 20:12:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ useEffect(()=>{
 console.log(videos)
   return (
     <div className="App">
-      <h1>Mearn Stack Clone</h1>
+    <img height={100} width={608} src="https://media.designrush.com/inspiration_images/136074/conversions/_1513706351_928_tinder1_bacbf6370ec8-mobile.jpg"></img>
       <div className='app-videos'>{videos.map(({url,channel,description,song,likes,shares,messages})=>(
       <Video key={url} url={url} 
       channel={channel} 
